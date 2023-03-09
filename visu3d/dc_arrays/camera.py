895e079601d069b2a9c2d6552a99179e281aec2d
Etienne Pot <epot@google.com>
2023-03-09 05:05:33 -0800

    Add compat.round
    PiperOrigin-RevId: 515304521

--- visu3d/dc_arrays/camera.py
+++ visu3d/dc_arrays/camera.py
@@ -189,7 +189,7 @@ class Camera(array_dataclass.DataclassArray):
     )
     rgb = rgb[valid_coords_mask]
     px_coords = px_coords[valid_coords_mask]
-    px_coords = enp.compat.astype(self.xnp.around(px_coords), np.int32)
+    px_coords = enp.compat.astype(enp.compat.round(px_coords), np.int32)
 
     # TODO(epot): Should we create a `xnp.asarray` ?
     # TODO(epot): The dtype should be cloned from point.rgb !
